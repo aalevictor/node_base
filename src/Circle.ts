24f88833b10f31081f0561f4cbd72ca05b4d31b7
Victor Abreu <aalevictor@gmail.com>
2023-01-19 12:06:40 -0300

    Correção da área, adição de calc de circunferência

--- src/Circle.ts
+++ src/Circle.ts
@@ -1,9 +1,15 @@
 export default class Circle {
+    constructor (readonly radius: number) {}
 
-  constructor (readonly radius: number) {
-  }
+    getArea() : number { //PI * r^2
+        return Math.PI * (this.radius * this.radius);
+    }
+    
+    getCircumference() : number { //2 * PI * r
+        return 2 * Math.PI * this.radius;
+    }
 
-   getArea() : number {
-       return 2 * Math.PI * this.radius;
-   }
+    getDiameter() : number { //2 * r
+        return 2 * this.radius;
+    }
 }
--- test/Circle.test.ts
+++ test/Circle.test.ts
@@ -4,3 +4,13 @@ test("Should calculate the area of circle", function () {
     const circle = new Circle(2);
     expect(circle.getArea()).toBe(12.566370614359172);
 });
+
+test("Should calculate the circumference of circle", function () {
+    const circle = new Circle(2);
+    expect(circle.getCircumference()).toBe(12.566370614359172);
+});
+
+test("Should calculate the diameter of circle", function () {
+    const circle = new Circle(2);
+    expect(circle.getDiameter()).toBe(4);
+});
